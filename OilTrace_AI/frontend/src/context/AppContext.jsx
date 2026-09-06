020cb51095a81a0d1a64f143330cd60b65226e40
sowmya-220803 <n220803@rguktn.ac.in>
2026-09-06 22:27:01 +0530

    changes updated

--- OilTrace_AI/frontend/src/context/AppContext.jsx
+++ OilTrace_AI/frontend/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://oil-trace-tn33.onrender.com';
 
 const AppContext = createContext();
 
